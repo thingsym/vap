e758eb03327c9e776f9027fffa637a2af28b6f72
thingsym <thingsym@gmail.com>
2016-09-09 02:00:59 +0900

    add scalaenv

---
+++ spec/localhost/scalaenv_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'shellwords'
+
+if property["scala_version"] != 0 && property["java_version"] != 0 then
+
+  [property["scala_version"]].each do |scala_version|
+    describe command("scalaenv versions | grep #{scala_version}") do
+      let(:disable_sudo) { true }
+      its(:stdout) { should match(/#{Regexp.escape(scala_version)}/) }
+    end
+  end
+
+  # describe command('/home/vagrant/.scalaenv/shims/scala -version') do
+  #   let(:disable_sudo) { true }
+  #   its(:stdout) { should match /#{Regexp.escape(property["scala_version"])}/ }
+  # end
+
+  describe command('scalaenv global') do
+    let(:disable_sudo) { true }
+    its(:stdout) { should match property["scala_version"] }
+  end
+
+  describe file('/home/vagrant/.bash_profile') do
+    its(:content) { should match /export PATH=\$HOME\/\.scalaenv\/bin:\$PATH/ }
+    its(:content) { should match /eval "\$\(scalaenv init \-\)"/ }
+  end
+
+  describe file('/home/vagrant/.bashrc') do
+    its(:content) { should match /export PATH=\$HOME\/\.scalaenv\/bin:\$PATH/ }
+    its(:content) { should match /eval "\$\(scalaenv init \-\)"/ }
+  end
+
+end
