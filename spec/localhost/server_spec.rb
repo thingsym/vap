9ee13c80a03d732de5365e7c2613aaabd216bafc
thingsym <thingsym@gmail.com>
2016-11-15 22:45:43 +0900

    fix tests by OS distribution

--- spec/localhost/server_spec.rb
+++ spec/localhost/server_spec.rb
@@ -3,30 +3,51 @@ require 'shellwords'
 
 if property["server"] == 'apache' then
 
-  describe package('httpd') do
+  describe package('httpd'), :if => os[:family] == 'redhat' do
     it { should be_installed }
   end
 
-  describe package('httpd-devel') do
+  describe package('httpd-devel'), :if => os[:family] == 'redhat' do
     it { should be_installed }
   end
 
-  describe service('httpd') do
+  describe service('httpd'), :if => os[:family] == 'redhat' do
     it { should be_enabled }
     it { should be_running }
   end
 
-  describe file('/etc/httpd/conf/httpd.conf') do
+  describe file('/etc/httpd/conf/httpd.conf'), :if => os[:family] == 'redhat' do
     it { should be_file }
   end
 
-  describe file('/etc/httpd/conf.d/www.conf'), :if => os[:release] == '7' do
+  describe file('/etc/httpd/conf.d/www.conf'), :if => os[:family] == 'redhat' && os[:release] == '7' do
+    it { should be_file }
+  end
+
+  describe package('apache2'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
+    it { should be_installed }
+  end
+
+  describe package('apache2-dev'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
+    it { should be_installed }
+  end
+
+  describe service('apache2'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file('/etc/apache2/apache2.conf'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
+    it { should be_file }
+  end
+
+  describe file('/etc/apache2/envvars'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
     it { should be_file }
   end
 
 elsif property["server"] == 'nginx' then
 
-  describe yumrepo('nginx') do
+  describe yumrepo('nginx'), :if => os[:family] == 'redhat' do
     it { should exist }
   end
 
