e08dc7bc485ed0b618fb3fc87b4a5f2e61b5a67e
thingsym <thingsym@gmail.com>
2017-01-25 02:40:40 +0900

    fix tests

--- spec/localhost/develop_tools_spec.rb
+++ spec/localhost/develop_tools_spec.rb
@@ -17,7 +17,15 @@ describe package('software-properties-common'), :if => os[:family] == 'debian' |
   it { should be_installed }
 end
 
-describe package('python-pip') do
+describe package('python2-pip'), :if => os[:family] == 'redhat' && os[:release] == '7' do
+  it { should be_installed }
+end
+
+describe package('python-pip'), :if => os[:family] == 'redhat' && os[:release] == '6' do
+  it { should be_installed }
+end
+
+describe package('python-pip'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
   it { should be_installed }
 end
 
