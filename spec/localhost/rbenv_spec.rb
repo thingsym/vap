ff89e6993f614858fcdd9a79687b17c19747f014
thingsym <thingsym@gmail.com>
2021-07-22 22:33:25 +0900

    fix: rename bashrc alias

--- spec/localhost/rbenv_spec.rb
+++ spec/localhost/rbenv_spec.rb
@@ -37,7 +37,7 @@ if property["ruby_version"] != 0 then
     its(:stdout) { should match property["ruby_version"] }
   end
 
-  describe file('/home/vagrant/.bashrc_vap') do
+  describe file('/home/vagrant/.bashrc_alias') do
     its(:content) { should match /export PATH=\$HOME\/\.rbenv\/bin:\$PATH/ }
     its(:content) { should match /eval "\$\(rbenv init \-\)"/ }
   end
